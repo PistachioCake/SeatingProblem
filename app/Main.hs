61b67abdb632b009b2e7e1dc68143fad6bf3daa5
PistachioCake <rushil.udani@gmail.com>
2019-06-03 22:04:22 -0500

    Actually add functionality to the solution. It should now theoretically work. Also, I refactored a bit.
    After much work, the solution should now work. One problem: It is *UNBELIEVABLY* slow. I don't think it's feasible to use this yet. Also, I changed some of the code to look cleaner / run faster.

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,22 @@
 module Main where
 
 import Lib
+import System.Environment (getArgs)
+import System.Exit (exitSuccess)
+import Control.Monad (forever)
 
 main :: IO ()
 main = do
-    putStrLn "HI!\n" 
-    let sol = maybe [] id $ solve [] [] []
-    print $ all (== (head sol)) sol
-    print sol
+    putStrLn "Table Size? > "
+    tableSize <- read <$> getLine
+    putStrLn "Class Size? > "
+    classSize <- read <$> getLine
+    putStrLn "Num Classes? > "
+    numClasses <- read <$> getLine
+    -- let numClasses = (tableSize * classSize - 1) `div` (tableSize - 1)
+    let sol = {- maybe [] id $ -} solve tableSize classSize numClasses [] [] []
+    -- print (length sol)
+    putStrLn (showSolution $ head sol)
+    forever $ do
+        a <- getLine
+        if a == "q" then exitSuccess else return ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,10 @@
 module Lib
-    ( solve, Person, Table, Class
+    ( solve, Person, Table, Class, showSolution
     ) where
 
 import Data.Traversable (for)
-import Control.Monad (MonadPlus (..))
+import Data.List (intercalate)
+import Control.Monad (MonadPlus (..), forM_, guard)
 import Debug.Trace (trace)
 
 -- | A Type to represent a person, just for ease of change
@@ -18,33 +19,49 @@ addOne p table
   | length table < 4 = Just $ p : table
   | otherwise = Nothing
 
-allPeople :: (MonadPlus m) => m Person
-allPeople = foldl (flip (mplus . return)) mzero $ map Person [1..28]
 
-tableSize = 4
-classSize = 7
-numClasses = 9
+fromList :: (MonadPlus m) => [a] -> m a
+fromList = foldl (flip (mplus . return)) mzero
 
-solve :: (MonadPlus m) => Table -> Class -> [Class] -> m [Class]
-solve incTable incClass madeClasses = {- trace (show (length madeClasses) ++ show incClass ++ show incTable ++ "\n\n") $  -}do
-    person <- fromList . filter (\p -> p `notElem` incTable && p `notElem` concat incClass) $ allPeople
-    
-    let madeTables = filter (elem person) madeClasses
+solve :: Int -> Int -> Int -> Table -> Class -> [Class] -> [[Class]]
+solve tableSize classSize numClasses = solve' 
+    where
+        allPeople :: [Person]
+        allPeople = map Person [1..tableSize * classSize]
 
+        solve' :: Table -> Class -> [Class] -> [[Class]]
+        solve' incTable incClass madeClasses = {- trace (show (length madeClasses) ++ show incClass ++ show incTable ++ "\n\n") $  -}do
+            person <- filter (\p -> p `notElem` incTable && p `notElem` concat incClass) $ allPeople
+            trace ("Person: " ++ showPerson person ++ "\nTable: " ++ showTable incTable ++ "\nClass: " ++ showClass incClass ++ "\nMadeClasses: \n" ++ showSolution madeClasses ++ "\n--------\n\n") [()]
+            let madeTables = filter (elem person) . concat $ madeClasses -- all Tables that have person in them
+            -- trace (show person) [()]
+            forM_ incTable (\partner ->
+                (guard $ null $ filter (elem partner) madeTables))
+            
+            if length incTable == tableSize - 1 then 
+                if length incClass == classSize - 1 then
+                    if length madeClasses == (numClasses - 1) then
+                        return (((person:incTable):incClass):madeClasses)
+                    else solve' [] [] (((person:incTable):incClass):madeClasses)
+                else solve' [] ((person:incTable):incClass) madeClasses
+            else solve' (person:incTable) incClass madeClasses
+            
+            {- 
+            let incTable' = person : incTable in if length incTable' == 4 then 
+                let incClass' = incTable' : incClass in if length incClass' == 7 then
+                    let madeClasses' = incClass' : madeClasses in if length madeClasses' == 9 then return madeClasses'
+                    else solve' [] [] madeClasses'
+                else solve' [] incClass' madeClasses
+            else solve' incTable' incClass madeClasses -}
     
-    if length incTable == tableSize - 1 then 
-        if length incClass == classSize - 1 then
-            if length madeClasses == (numClasses - 1) then
-                return (((person:incTable):incClass):madeClasses)
-            else solve [] [] (((person:incTable):incClass):madeClasses)
-        else solve [] ((person:incTable):incClass) madeClasses
-    else solve (person:incTable) incClass madeClasses
-    
-    {- 
-    let incTable' = person : incTable in if length incTable' == 4 then 
-        let incClass' = incTable' : incClass in if length incClass' == 7 then
-            let madeClasses' = incClass' : madeClasses in if length madeClasses' == 9 then return madeClasses'
-            else solve [] [] madeClasses'
-        else solve [] incClass' madeClasses
-    else solve incTable' incClass madeClasses -}
-    
+showSolution :: [Class] -> String
+showSolution sol = intercalate "\n" $ map showClass sol
+
+showClass :: Class -> String
+showClass cl = intercalate " | " $ map showTable cl
+
+showTable :: Table -> String
+showTable table = intercalate " " $ map showPerson table
+
+showPerson :: Person -> String
+showPerson (Person p) = show p
